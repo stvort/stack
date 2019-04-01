ee9e361726e37960a24b69ac79df586421f587b3
stvort <stvort@mail.ru>
2019-04-01 22:57:09 +0400

    projects renaming

--- MongoDBDemo/src/main/java/ru/otus/example/mongodbdemo/repositories/StudentRepositoryCustom.java
+++
@@ -1,6 +0,0 @@
-package ru.otus.example.mongodbdemo.repositories;
-
-public interface StudentRepositoryCustom {
-    long getExperienceArrayLengthByStudentId(String id);
-    void removeExperienceArrayElementsById(String id);
-}
